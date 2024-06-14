8f9e6a2c00e0c49058f71e0f607e50276eaa6e02
Mohammed Nabil <Nabil1243@outlook.com>
2024-06-14 12:33:30 -0700

    added restaurant search endpoint

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
 import myRestaurantRoute from "./routes/MyRestaurantRoute";
+import restaurantRoute from "./routes/RestaurantRoute";
 import { v2 as cloudinary } from "cloudinary";
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
@@ -25,6 +26,7 @@ app.get("/health", async (req: Request, res: Response) => {
 
 app.use("/api/my/user", myUserRoute);
 app.use("/api/my/restaurant", myRestaurantRoute);
+app.use("/api/restaurant", restaurantRoute);
 
 app.listen(7020, () => {
   console.log("Server started on localhost:7020");
---
+++ src/routes/RestaurantRoute.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import { param } from "express-validator";
+import RestaurantController from "../controllers/RestaurantController";
+
+const router = express.Router();
+
+// /api/restaurant/search/london
+router.get(
+  "/search/:city",
+  param("city")
+    .isString()
+    .trim()
+    .notEmpty()
+    .withMessage("City parameter must be a valid string"),
+
+  RestaurantController.searchRestaurant
+);
+
+export default router;
